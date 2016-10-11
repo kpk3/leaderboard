2e58bbf517fcc0d0b4d56dae2e274fe1df9a67b6
kpk3 <kpk3@hawaii.edu>
2016-10-11 10:15:34 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,3 +1,27 @@
 /**
  * Created by junoa on 10/11/2016.
  */
+PlayersList = new Mongo.Collection('tasks');
+if(Meteor.isServer){
+
+}
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    }
+  });
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+}
